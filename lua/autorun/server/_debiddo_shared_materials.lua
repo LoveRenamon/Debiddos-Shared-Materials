225de8ffc056b439a9ce650e05cb2d5c68cea7c8
LoveRenamon <love_renamon@hotmail.com>
2021-12-14 22:55:22 -0300

    added some ref proxies 	related to 		Pupil 		BurnLevel 		BBQLevel 		BloodyHands 		IT

---
+++ lua/autorun/server/_debiddo_shared_materials.lua
@@ -0,0 +1,2 @@
+resource.AddWorkshop("2086033839")
+MsgC( Color( 35, 245, 13 ), "[Debiddo's Shared Materials] Loaded!\nEnforced \"Debiddo's Shared Materials\" for all Clients!" )
---
+++ lua/matproxy/LightAmount.lua
@@ -0,0 +1,10 @@
+--[[
+matproxy.Add({
+	name = "LightAmount",
+	init = function( self, mat, values )
+		self.ResultTo = values.resultVar
+	end,
+	bind = function( self, mat, ent )
+	end
+})
+--]]
