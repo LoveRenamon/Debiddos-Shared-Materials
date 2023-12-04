dc76c9d5baa1a88efb439f977206ecc113da60fc
LoveRenamon <love_renamon@hotmail.com>
2023-12-04 12:51:16 -0300

    add some Proxies which are present on Steam Workshop version
    	new file:   lua/matproxy/_animetone.lua
    	new file:   lua/matproxy/_tonemap.lua
    	new file:   lua/matproxy/convar.lua

--- lua/matproxy/_tonemap.lua
+++ lua/matproxy/_tonemap.lua
@@ -1,23 +1,20 @@
--- ToneMap pegar o valor linear da Convar mat_hdr_tonemapscale
+-- ToneMap pega o valor linear da Convar mat_hdr_tonemapscale
 matproxy.Add( {
    name = "ToneMap",
 
    init = function( self, mat, values )
       self.ResultXYZ = values.resultvar
-      -- self.ResultFloat = values.float
    end,
 
-   bind = function( self, mat, ent )
+   bind = function( self, mat, ent ) -- TODO: lower ticks
       if not (IsValid( ent )) then return end
 
       local XYZ = render.GetToneMappingScaleLinear()
-      -- local LF = XYZ * 0.0625
       local R = XYZ.x * 1.5127
       local G = XYZ.y * 1.4413
       local B = XYZ.z * 2.2651
 
       mat:SetVector( self.ResultXYZ, Vector( R, G, B ) )
-      -- mat:SetFloat( self.ResultFloat, XYZ.x )
 
    end
 } )
